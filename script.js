e109f7e8f9c4cbcc1ee5b96cab06fc60e2b90b3e
Cynth <cynthia.enciso28@gmail.com>
2021-11-30 08:59:45 -0600

    add second HTML form

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-// convert to and from decimal and binary
+// #1. Make sure that if one dropdown has a value, the other dropdown isn't the same value
 
-// convert to and from binary and hexadecimal
+// #2. conversions for each: 
+    // binary to decimal
+    // decimal to binary
+
+    // binary to hex
+    // hex to binary
+
+    // hex to decimal
+    // decimal to hex
+
+// #3. random value generator logic and correct answer
